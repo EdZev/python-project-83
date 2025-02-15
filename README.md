bf07f4d68a7e43b25f5adc25c93495c51b581bb5
Edker <p89028084234@gmail.com>
2025-02-15 18:31:04 +0500

    step 2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/EdZev/python-project-83/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/EdZev/python-project-83/actions)
+
+### Deploy URL
+https://python-project-83-ythf.onrender.com
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,18 +1,24 @@
+import os
 from flask import Flask
 from dotenv import load_dotenv
-import os
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 load_dotenv()
-
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
 
 
+env = Environment(
+    loader=PackageLoader("page_analyzer"),
+    autoescape=select_autoescape(['html'])
+)
+template = env.get_template('layout.html')
+
+
 @app.route('/')
 def hello():
-    return "Hello, Flask!"
+    return template.render()
 
 
 if __name__ == '__main__':
